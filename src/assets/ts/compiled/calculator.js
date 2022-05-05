ecc99e8ea702afa2542ff3609c8b4063ec3eb394
davicesca <davifagundescesca@gmail.com>
2022-05-05 14:13:52 -0300

    Add 'addNumber' method functionality

--- src/assets/ts/compiled/calculator.js
+++ src/assets/ts/compiled/calculator.js
@@ -1,5 +1,23 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Calculator {
+export default class Calculator {
+    constructor(previousOperandTxt, currentOperandTxt) {
+        this.previousOperandTxt = previousOperandTxt;
+        this.currentOperandTxt = currentOperandTxt;
+        this.clear();
+    }
+    clear() {
+        this.previousOperand = '';
+        this.currentOperand = '';
+        this.operation = '';
+    }
+    delete() { }
+    addNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.'))
+            return;
+        this.currentOperand = this.currentOperand + number;
+    }
+    selectOperation(operation) { }
+    calculate() { }
+    render() {
+        this.currentOperandTxt.innerText = this.currentOperand;
+    }
 }
-exports.default = Calculator;
--- src/assets/ts/script.ts
+++ src/assets/ts/script.ts
@@ -1,1 +1,16 @@
+// Importing Calculator class
 import Calculator from './calculator';
+
+const calculatorContainer = document.querySelector('[data-calculator-container]') as HTMLDivElement;
+const previousOperandTxt = document.querySelector('[data-previous-operand]') as HTMLDivElement;
+const currentOperandTxt = document.querySelector('[data-current-operand]') as HTMLDivElement;
+
+const calculator = new Calculator(previousOperandTxt, currentOperandTxt);
+
+calculatorContainer.addEventListener('click', (event) => {
+    const target = event.target as HTMLInputElement;
+    if(target.hasAttribute('data-number')) {
+        calculator.addNumber(target.innerText);
+        calculator.render();
+    }
+});
